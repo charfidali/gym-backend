79829cb4c28d784d83bf7a1ca7bbb229270c8f17
charfidali <mohamedali.charfi@esprit.tn>
2026-01-22 00:34:48 +0100

    update runapp.sh

--- run-app.sh
+++ run-app.sh
@@ -2,6 +2,8 @@
 set -e
 
 echo "Running migrations..."
+export DATABASE_URL="mysql://$MYSQLUSER:$MYSQLPASSWORD@$MYSQLHOST:$MYSQLPORT/$MYSQLDATABASE"
+
 php bin/console doctrine:migrations:migrate --no-interaction || true
 
 echo "Starting server..."
