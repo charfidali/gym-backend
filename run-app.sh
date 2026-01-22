68152bd6bdcd31a95dc6d893fbda1a5c158afba7
charfidali <mohamedali.charfi@esprit.tn>
2026-01-22 01:00:33 +0100

    update runapp.sh

--- run-app.sh
+++ run-app.sh
@@ -2,15 +2,11 @@
 set -e
 
 echo "Running migrations..."
-#!/bin/bash
-# wait for MySQL to be ready
-until php bin/console doctrine:query:sql "SELECT 1" >/dev/null 2>&1; do
-  echo "Waiting for MySQL..."
-  sleep 2
-done
 
 # run migrations safely
 php bin/console doctrine:migrations:migrate --env=prod --no-interaction
+
+echo " migrations done"
 
 # start server
 php -S 0.0.0.0:$PORT -t public
